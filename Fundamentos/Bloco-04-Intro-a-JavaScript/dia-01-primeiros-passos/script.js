86f488bcb52d0861780fa1e6a2770473d1e2726c
Cesar Xavier <cxavier.vet@gmail.com>
2022-04-08 11:22:16 -0300

    Programa que compara o maior entre 3 números

--- Fundamentos/Bloco-04-Intro-a-JavaScript/dia-01-primeiros-passos/script.js
+++ Fundamentos/Bloco-04-Intro-a-JavaScript/dia-01-primeiros-passos/script.js
@@ -1,11 +1,17 @@
-const a = 12
-const b = 15
+const a = 21
+const b = 19
+const c = 12
 
-if (b > a) {
-    console.log ("B é maior que A. Confira: ")
-} else {
-    console.log ("A é maior ou igual a B. Confira: ")
+if (b > a && b > c) {
+    console.log ("B é maior que A e C Confira: ")
+} 
+else if (a > b && a > c) {
+    console.log ("A é maior que B e C. Confira: ")
+} else if (c > a && c > b) {
+    console.log ("C é maior que B e C. Confira: ")
 }
+
 console.log ("")
 console.log ("A é igual a: " + a)
 console.log ("B é igual a: " + b)
+console.log ("C é igual a: " + c)
