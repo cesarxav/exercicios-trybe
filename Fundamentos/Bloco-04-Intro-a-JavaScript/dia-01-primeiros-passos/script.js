8bafbb7052fca401f4c79985f469a44f3aea8073
Cesar Xavier <cxavier.vet@gmail.com>
2022-04-08 11:05:42 -0300

    Cinco programadas de operação aritmética básica

---
+++ Fundamentos/Bloco-04-Intro-a-JavaScript/dia-01-primeiros-passos/script.js
@@ -0,0 +1,11 @@
+let weekDay = ""
+
+if(weekDay == "segunda-feira" || "terça-feira" || "quarta-feira" || "quinta-feira" || "sexta-feira") {
+    console.log ("Obá, mais um dia de aprendizado na Trybe! >:D")
+}
+else if (weekDay == "sábado" || "domingo") {
+    console.log ("Fim de semana caraioo ! vamo bebê !")
+} else {
+   console.log ("Não é um dia da semana válido !")
+}
+    
