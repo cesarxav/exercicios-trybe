640d4307e238eada7582bc61db3effec46ff44e6
Cesar Xavier <cxavier.vet@gmail.com>
2022-04-08 18:28:50 -0300

    Exercício para determinar se existe algum numero par com booleanos

--- Fundamentos/Bloco-04-Intro-a-JavaScript/dia-01-primeiros-passos/script.js
+++ Fundamentos/Bloco-04-Intro-a-JavaScript/dia-01-primeiros-passos/script.js
@@ -1,23 +1,12 @@
-let nota = 400
-if (nota > 100 || nota < 0) {
-    console.log ("Erro!")
-}
-else if (nota >= 90) {
-    console.log ("Sua nota foi: A")
-}
-else if (nota >= 80) {
-    console.log ("Sua nota foi: B")
-}
-else if (nota >= 70) {
-console.log ("Sua nota foi: C")
-}
-else if (nota >= 60) {
-console.log ("Sua nota foi: D")
-}
-else if (nota>= 50) {
-console.log ("Sua nota foi: E")
-} else {
-    console.log ("Sua nota foi: F")
+const a = 10
+const b = 6
+const c = 5
+
+let par = false
+
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
+    par = true
+    console.log (par + ". Pelo menos um de seus números é par!") 
 }
 
-
+    
