ea3387f773aea0964b320d474ba208e89089e126
Cesar Xavier <cxavier.vet@gmail.com>
2022-04-08 12:04:59 -0300

    If dentro de IF.. Programa que retorna se ângulos são de triângulo ou não

--- Fundamentos/Bloco-04-Intro-a-JavaScript/dia-01-primeiros-passos/script.js
+++ Fundamentos/Bloco-04-Intro-a-JavaScript/dia-01-primeiros-passos/script.js
@@ -1,10 +1,14 @@
-const valor = 9
-
-if (valor > 0) {
-    console.log ("O SEU NÚMERO É POSITIVO !")
-}
-else if (valor < 0) {
-    console.log ("SEU NÚMERO É NEGATIVO !")
+const anguloA = -23
+const anguloB = 98
+const anguloC = 50
+let soma = (anguloA + anguloB + anguloC)
+let positivos = anguloA > 0 && anguloB > 0 && anguloC > 0
+if (positivos) {
+    if (soma == 180) {
+    console.log (true)
+} else { 
+    console.log (false)
+ }; 
 } else {
-    console.log ("SEU NÚMERO É IGUAL A ZERO !")
+    console.log ("Não é um ângulo válido")
 }
