97e1011c265677585b75f59bcc37893162fdf851
Cesar Xavier <cxavier.vet@gmail.com>
2022-04-08 13:53:23 -0300

    Programa de peças de xadrez e direções

--- Fundamentos/Bloco-04-Intro-a-JavaScript/dia-01-primeiros-passos/script.js
+++ Fundamentos/Bloco-04-Intro-a-JavaScript/dia-01-primeiros-passos/script.js
@@ -1,14 +1,31 @@
-const anguloA = -23
-const anguloB = 98
-const anguloC = 50
-let soma = (anguloA + anguloB + anguloC)
-let positivos = anguloA > 0 && anguloB > 0 && anguloC > 0
-if (positivos) {
-    if (soma == 180) {
-    console.log (true)
-} else { 
-    console.log (false)
- }; 
-} else {
-    console.log ("Não é um ângulo válido")
-}
+const peça = "BiSpO"
+
+switch (peça.toLowerCase()) {
+    case "rainha":
+        console.log ("Sua peça: " + peça + " --> Se move em rodas as direções")
+        break;
+
+    case "bispo":
+    console.log ("Sua peça: " + peça + " --> Anda na diagonal")
+        break;
+        
+    case "peão":
+    console.log ("Sua peça: " + peça + " --> Anda somente para frente")
+        break;
+
+    case "torre":
+    console.log ("Sua peça: " + peça + " --> Anda para os lados, para frente e para trás")
+        break;
+
+    case "rei":
+    console.log ("Sua peça: " + peça + " --> Anda uma casa em qualquer direção")
+        break;
+
+    case "cavalo" :
+    console.log ("Sua peça: " + peça + " --> Anda em L")
+        break;
+
+    default:
+    console.log ("Erro, peça inválida!")
+        break;
+};
