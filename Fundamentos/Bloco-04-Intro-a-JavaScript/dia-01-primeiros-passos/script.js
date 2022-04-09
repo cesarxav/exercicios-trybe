9ebde44bd44151ebc92d1f3428bc5d63c0dca6b0
Cesar Xavier <cxavier.vet@gmail.com>
2022-04-08 21:40:04 -0300

    Programa para calcular salario base descontando alíquota INSS e IR de acordo com o salário bruto

--- Fundamentos/Bloco-04-Intro-a-JavaScript/dia-01-primeiros-passos/script.js
+++ Fundamentos/Bloco-04-Intro-a-JavaScript/dia-01-primeiros-passos/script.js
@@ -1,16 +1,29 @@
-const custoProduto = 5
-const valorDeVenda = 10
-let quantidade = 1000
+let salbruto = 5000
+let valinss;
+let valir
 
-let custoComImposto = custoProduto * 1.2
-let lucro = (valorDeVenda - custoComImposto) * 1000
-
-if (custoProduto <= 0 || valorDeVenda <= 0) {
-    console.log ("Erro nos valores de entrada")
+if (salbruto <= 1556.94) {
+    valinss = (salbruto * 0.08)
+} else if (salbruto <= 2594.92 ) {
+    valinss = (salbruto * 0.09)
+} else if (salbruto <= 5189.82) {
+    valinss = (salbruto * 0.11)
 } else {
-    console.log ("A empresa obteve R$" + lucro + " de lucro com a venda de 1000 produtos")
+    valinss = 570.88
 }
 
+const salbase = salbruto - valinss;
 
+if (salbase <= 1903.98) {
+    valir = 0
+} else if (salbase <= 2826.65) {
+    valir = (0.075 * salbase) - 142.80
+} else if (salbase <= 3751.05) {
+    valir = (0.15 * salbase) - 354.80
+} else if (salbase <= 4664.68) {
+    valir = (0.225 * salbase) - 636.13
+} else {
+    valir = (0.275 * salbase) - 869.36
+}
 
-
+console.log ("O seu salário é de: R$" + (salbase - valir))
