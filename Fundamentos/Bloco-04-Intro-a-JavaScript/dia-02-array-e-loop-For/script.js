4c5dd253476e9914dc500352a41f457c6014ae9e
Cesar Xavier <cxavier.vet@gmail.com>
2022-04-10 18:53:00 -0300

    Descobrindo o maior número de um array usando For - minha forma e a do gabarito

--- Fundamentos/Bloco-04-Intro-a-JavaScript/dia-02-array-e-loop-For/script.js
+++ Fundamentos/Bloco-04-Intro-a-JavaScript/dia-02-array-e-loop-For/script.js
@@ -1,13 +1,25 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let soma = 0
-    for (i = 0; i < numbers.length; i += 1) {
-        soma += numbers[i]
-    }
-    let media = soma / numbers.length;
-    console.log (media)
+//Maneira que eu fiz
+let numbers = [200, 105, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNumero = 0;
 
-if (media > 20) {
-    console.log ("Valor maior que 20 !")
-} else {
-    console.log ("Valor menor ou igual a 20")
-}
+    for (i = 0; i < numbers.length; i += 1) {
+        if (numbers[i] > maiorNumero) {
+            maiorNumero = numbers[i]
+        }
+    }
+
+    console.log(maiorNumero)
+
+/* - Maneira como o exercício mostrou no gabarito para corrigir o erro de que a variável Maior Número não pode ser maior que nenhum valor dentro do array ! 4
+
+let numbers = [200, 105, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let maiorNumero = numbers[0]
+
+    for (i = 0; i < numbers.length; i += 1) {
+        if (numbers[i] > maiorNumero) {
+            maiorNumero = numbers[i]
+        }
+    }
+
+    console.log(maiorNumero)
+        */
