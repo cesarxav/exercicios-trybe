bdc7b29c49a876a3040d7c1195d5b6cb13638c30
Cesar Xavier <cxavier.vet@gmail.com>
2022-04-10 19:08:40 -0300

    Descorindo quantos números ímpares no array

--- Fundamentos/Bloco-04-Intro-a-JavaScript/dia-02-array-e-loop-For/script.js
+++ Fundamentos/Bloco-04-Intro-a-JavaScript/dia-02-array-e-loop-For/script.js
@@ -1,25 +1,15 @@
 //Maneira que eu fiz
-let numbers = [200, 105, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maiorNumero = 0;
+let numbers = [200, 105, 9, 3, 19, 70, 8, 100, 2, 35, 27, 13];
+let quantosImpares = 0
 
     for (i = 0; i < numbers.length; i += 1) {
-        if (numbers[i] > maiorNumero) {
-            maiorNumero = numbers[i]
+        if (numbers[i] % 2 !== 0) {
+        quantosImpares += 1 
         }
     }
 
-    console.log(maiorNumero)
-
-/* - Maneira como o exercício mostrou no gabarito para corrigir o erro de que a variável Maior Número não pode ser maior que nenhum valor dentro do array ! 4
-
-let numbers = [200, 105, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maiorNumero = numbers[0]
-
-    for (i = 0; i < numbers.length; i += 1) {
-        if (numbers[i] > maiorNumero) {
-            maiorNumero = numbers[i]
-        }
+    if (quantosImpares == 0) {
+        console.log ("Não há numeros ímpares")
+    } else {
+        console.log ("São " + quantosImpares + " números ímpares")
     }
-
-    console.log(maiorNumero)
-        */
