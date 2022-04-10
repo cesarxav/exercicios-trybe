7a4198a14526c590b46a9f4e69c7d367a31a562f
Cesar Xavier <cxavier.vet@gmail.com>
2022-04-10 20:02:28 -0300

    Criando um array que vai do 1 ao 25 com o loop for

--- Fundamentos/Bloco-04-Intro-a-JavaScript/dia-02-array-e-loop-For/script.js
+++ Fundamentos/Bloco-04-Intro-a-JavaScript/dia-02-array-e-loop-For/script.js
@@ -1,12 +1,7 @@
-// baseado no exercício de encontrar o maior número !
-let numbers = [1, 200, 105, 9, 3, 19, 70, 8, 100, 2, 35, 27, 13];
-let menorNumero = numbers[0]
+let numbers = []
 
-    for (i = 1; i < numbers.length; i += 1) {
-        if (numbers[i] < menorNumero) {
-            menorNumero = numbers[i]
-        }
+for (i = 1; i <= 25; i++) {
+    numbers.push(i);
+}
 
-       }
-       console.log (menorNumero)
-    
+console.log(numbers)
